28235f39576e439dcf17feea886d91cef859cfe6
AdaDev11 <you@example.com>
2025-01-17 01:31:50 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import PrimarySearchAppBar from "./components/PrimarySearchAppBar.tsx";
 import ImgMediaCard from "./components/SimpleContainer.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -6,8 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <QueryClientProvider client={queryClient}>
             <PrimarySearchAppBar />
--- src/components/SimpleContainer.tsx
+++ src/components/SimpleContainer.tsx
@@ -1,19 +1,14 @@
 import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import ImgMediaCard from "./ImgMediaCard.tsx";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
-// import * as React from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
-import Masonry from "@mui/lab/Masonry";
 
 const fetchProducts = async () => {
     const res = await axios.get("https://api.escuelajs.co/api/v1/products");
